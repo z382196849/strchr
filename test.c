41633197c9b7ea12e76cd8e74907322f15b2dcfc
z382196829 <1936851315@qq.com>
2021-12-19 01:09:49 +0800

    第三十次提交

---
+++ test.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<windows.h>
+#include<string.h>
+
+char* my_strchr(char* str, char c)
+{
+    if(*str == '\0')
+    {
+  	return NULL;
+    }
+    while(*str != '\0')
+    {
+	if(*str == c)
+	{
+	    printf("找到了\n");
+	    return (char*) str;
+	}
+	str++;
+    }
+}
+
+void main()
+{
+    char str[] = "abcdef";
+    char c = 'c';
+    char* ret = my_strchr(str, c);
+    printf("ret = %s\n", ret);
+    system("pause");
+}
